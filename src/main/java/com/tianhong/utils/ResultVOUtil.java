68c7ce5135df137846ddf04e0896d93466ae367d
tianhong229 <wth@tianhongs-MBP.lan>
2018-06-01 17:42:53 +0800

    add resultVO utils

--- src/main/java/com/tianhong/utils/ResultVOUtil.java
+++ src/main/java/com/tianhong/utils/ResultVOUtil.java
@@ -11,4 +11,15 @@ public class ResultVOUtil {
         resultVO.setMsg("成功");
         return resultVO;
     }
+
+    public static ResultVO success() {
+        return success(null);    
+    }
+
+    public static ResultVO error(Integer code, String msg) {
+            ResultVO resultVO = new ResultVO();
+            resultVO.setCode(code);
+            resultVO.setMsg(msg);
+            return resultVO;
+    }
 }
